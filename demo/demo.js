e288658b1f9a8f275dd9918eb6e35b92a62f8e5b
leopoldthecoder <leopoldthecuber@gmail.com>
2015-07-14 21:47:36 +0800

    Init demo

---
+++ demo/demo.js
@@ -0,0 +1,3 @@
+﻿window.onload = function() {
+  perspective.scroll([["transform", "translateY(0px)", "-100"], ["transform", "translateY(0px)", "-200"], ["transform", "translateY(0px)", "-500"]], [2]);
+}
